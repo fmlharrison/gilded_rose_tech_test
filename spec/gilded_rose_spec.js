236943576e7fe6ed802fe7915db0b8512f559b4f
Felix Harrison <fmlharrison@gmail.com>
2016-12-01 14:18:51 +0000

    refeactored into classes

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,5 +1,6 @@
 describe("Gilded Rose", function() {
 
+  var gildedRose;
   var item;
   var brie;
   var backstage_pass;
@@ -8,90 +9,104 @@ describe("Gilded Rose", function() {
   var old_item;
 
   beforeEach(function () {
-    item = [ new Item("Mac'n'Cheese", 5, 20) ];
-    old_item = [new Item("Rottern Cabbage", -5, 0)]
-    brie_deluxe = [ new Item("Aged Brie", 10, 50)];
-    brie = [ new Item("Aged Brie", 15, 10)];
-    backstage_pass = [ new Item('Backstage passes to a TAFKAL80ETC concert', 9, 20)];
-    sulfuras = [ new Item('Sulfuras, Hand of Ragnaros', 0, 80)];
+    gildedRose = new GildedRose();
+    item = new Item("Mac'n'Cheese", 5, 20);
+    old_item = new Item("Rottern Cabbage", -5, 0);
+    brie_deluxe = new Item("Aged Brie", 10, 50);
+    brie = new Item("Aged Brie", 15, 10);
+    backstage_pass = new Item('Backstage passes to a TAFKAL80ETC concert', 9, 20);
+    sulfuras = new Item('Sulfuras, Hand of Ragnaros', 0, 80);
   });
 
   describe("updating normal food quality", function () {
 
+    beforeEach(function () {
+      gildedRose.addItem(item);
+    });
+
     it("should reduce the quality of a normal object", function() {
-      update_quality(item);
-      expect(item[0].quality).toEqual(19);
+      gildedRose.update_quality();
+      expect(gildedRose.items[0].quality).toEqual(19);
     });
 
     it("should reduce the sell in value of an object", function () {
-      update_quality(item);
-      expect(item[0].sell_in).toEqual(4);
-    });
-
-    it("quality can't go higher than 50", function () {
-      update_quality(brie_deluxe);
-      expect(brie_deluxe[0].quality).toEqual(50);
-    });
-
-    it("quality can't go lower than 0", function () {
-      update_quality(old_item);
-      expect(old_item[0].quality).toEqual(0);
+      gildedRose.update_quality();
+      expect(gildedRose.items[0].sell_in).toEqual(4);
     });
 
     it("quality reduces twice as fast after the sell_in date reaches 0", function () {
       for (var i = 0; i < 7; i++) {
-        update_quality(item);
+        gildedRose.update_quality();
       }
-      expect(item[0].quality).toEqual(11);
+      expect(gildedRose.items[0].quality).toEqual(11);
     });
 
   });
 
-  describe("Special cases", function () {
+  describe("edge cases", function () {
 
-    it("should know that brie gets better with age", function () {
-      update_quality(brie);
-      expect(brie[0].quality).toEqual(11);
+    beforeEach(function () {
+      gildedRose.addItem(brie_deluxe);
+      gildedRose.addItem(old_item);
     });
 
-    describe("back stage passes", function () {
-
-      it("increases in quality by 2 when there are 10 days left till sell in deadline", function () {
-        update_quality(backstage_pass);
-        expect(backstage_pass[0].quality).toEqual(22);
-      });
-
-      it("increases in quality by 3 when there are 5 days left till sell in deadline", function () {
-        for (var i = 0; i < 5; i++) {
-          update_quality(backstage_pass);
-        }
-        expect(backstage_pass[0].quality).toEqual(31);
-      });
-
-      it("looses all it's value after the sell in value reaches 0", function () {
-        for (var i = 0; i < 10; i++) {
-          update_quality(backstage_pass);
-        }
-        expect(backstage_pass[0].quality).toEqual(0);
-      });
-
+    it("quality can't go higher than 50", function () {
+      gildedRose.update_quality();
+      expect(gildedRose.items[0].quality).toEqual(50);
     });
 
-    describe("Sulfuras", function () {
-
-      it("doesn't decrease in value", function () {
-        update_quality(sulfuras);
-        expect(sulfuras[0].quality).toEqual(80);
-      });
-
-      it("doesn't have to be sold, it's everlasting", function () {
-        update_quality(sulfuras);
-        expect(sulfuras[0].sell_in).toEqual(0);
-      });
-
+    it("quality can't go lower than 0", function () {
+      gildedRose.update_quality();
+      expect(gildedRose.items[1].quality).toEqual(0);
     });
 
   });
+
+  // describe("Special cases", function () {
+  //
+  //   it("should know that brie gets better with age", function () {
+  //     update_quality(brie);
+  //     expect(brie[0].quality).toEqual(11);
+  //   });
+  //
+  //   describe("back stage passes", function () {
+  //
+  //     it("increases in quality by 2 when there are 10 days left till sell in deadline", function () {
+  //       update_quality(backstage_pass);
+  //       expect(backstage_pass[0].quality).toEqual(22);
+  //     });
+  //
+  //     it("increases in quality by 3 when there are 5 days left till sell in deadline", function () {
+  //       for (var i = 0; i < 5; i++) {
+  //         update_quality(backstage_pass);
+  //       }
+  //       expect(backstage_pass[0].quality).toEqual(31);
+  //     });
+  //
+  //     it("looses all it's value after the sell in value reaches 0", function () {
+  //       for (var i = 0; i < 10; i++) {
+  //         update_quality(backstage_pass);
+  //       }
+  //       expect(backstage_pass[0].quality).toEqual(0);
+  //     });
+  //
+  //   });
+  //
+  //   describe("Sulfuras", function () {
+  //
+  //     it("doesn't decrease in value", function () {
+  //       update_quality(sulfuras);
+  //       expect(sulfuras[0].quality).toEqual(80);
+  //     });
+  //
+  //     it("doesn't have to be sold, it's everlasting", function () {
+  //       update_quality(sulfuras);
+  //       expect(sulfuras[0].sell_in).toEqual(0);
+  //     });
+  //
+  //   });
+  //
+  // });
 
 
 });
--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -1,53 +1,53 @@
-function Item(name, sell_in, quality) {
-  this.name = name;
-  this.sell_in = sell_in;
-  this.quality = quality;
+function GildedRose() {
+  this.items = [];
 }
 
-var items = []
+GildedRose.prototype.addItem= function (item) {
+  this.items.push(item);
+};
 
-function update_quality(items) {
-  for (var i = 0; i < items.length; i++) {
-    if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-      if (items[i].quality > 0) {
-        if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-          items[i].quality = items[i].quality - 1
+GildedRose.prototype.update_quality = function () {
+  for (var i = 0; i < this.items.length; i++) {
+    if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+      if (this.items[i].quality > 0) {
+        if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+          this.items[i].quality = this.items[i].quality - 1
         }
       }
     } else {
-      if (items[i].quality < 50) {
-        items[i].quality = items[i].quality + 1
-        if (items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-          if (items[i].sell_in < 11) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
+      if (this.items[i].quality < 50) {
+        this.items[i].quality = this.items[i].quality + 1
+        if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+          if (this.items[i].sell_in < 11) {
+            if (this.items[i].quality < 50) {
+              this.items[i].quality = this.items[i].quality + 1
             }
           }
-          if (items[i].sell_in < 6) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
+          if (this.items[i].sell_in < 6) {
+            if (this.items[i].quality < 50) {
+              this.items[i].quality = this.items[i].quality + 1
             }
           }
         }
       }
     }
-    if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-      items[i].sell_in = items[i].sell_in - 1;
+    if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+      this.items[i].sell_in = this.items[i].sell_in - 1;
     }
-    if (items[i].sell_in < 0) {
-      if (items[i].name != 'Aged Brie') {
-        if (items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-          if (items[i].quality > 0) {
-            if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-              items[i].quality = items[i].quality - 1
+    if (this.items[i].sell_in < 0) {
+      if (this.items[i].name != 'Aged Brie') {
+        if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+          if (this.items[i].quality > 0) {
+            if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+              this.items[i].quality = this.items[i].quality - 1
             }
           }
         } else {
-          items[i].quality = items[i].quality - items[i].quality
+          this.items[i].quality = this.items[i].quality - this.items[i].quality
         }
       } else {
-        if (items[i].quality < 50) {
-          items[i].quality = items[i].quality + 1
+        if (this.items[i].quality < 50) {
+          this.items[i].quality = this.items[i].quality + 1
         }
       }
     }
