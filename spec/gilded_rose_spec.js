b43ec51ca4a8baa71cd580463424e8d6742d4502
Felix Harrison <fmlharrison@gmail.com>
2016-12-01 11:44:16 +0000

    testing brie

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,9 +1,26 @@
 describe("Gilded Rose", function() {
 
-  it("should foo", function() {
-    items = [ new Item("foo", 0, 0) ];
-    update_quality();
-    expect(items[0].name).toEqual("fixme");
+  var item;
+  var brie;
+
+  beforeEach(function () {
+    item = [ new Item("Mac'n'Cheese", 10, 20) ];
+    brie = [ new Item("Aged Brie", 15, 10)];
+  });
+
+  it("should reduce the quality of a normal object", function() {
+    update_quality(item);
+    expect(item[0].quality).toEqual(19);
+  });
+
+  it("should reduce the sell in value of an object", function () {
+    update_quality(item);
+    expect(item[0].sell_in).toEqual(9);
+  });
+
+  it("should know that brie gets better with age", function () {
+    update_quality(brie);
+    expect(brie[0].quality).toEqual(11);
   });
 
 });
